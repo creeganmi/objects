19cf3405e50a7f4c2cb7eec26ecd13c073b37332
Michael Creegan <creeg@AuroraR12.localdomain>
2022-05-27 21:12:08 -0400

    debug

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -4,7 +4,7 @@ from player import Player
 class Blackjack:
     def __init__(self):
         self.deck = Deck()
-        self.deck.generate()
+        self.deck.fiftytwo()
         self.player = Player(False, self.deck)
         self.dealer = Player(True, self.deck)
         
@@ -14,37 +14,37 @@ class Blackjack:
         
         self.player.show()
         
-        if palyer_status == 1:
+        if player_status == 1:
             print("Player's game - blackjack baby!")
             if dealer_status == 1:
                 print("Tie - Dealer and Player got blackjack!")
             return 1
         
-    cmd = ""                                    # this is where you play blackjack
-    while cmd != "Stay":
-        bust = 0
-        cmd = input("Hit or Stay?")
-        
-        if cmd == "Hit":
-            bus = self.player.hit()
-            self.player.show()
-        if bust == 1:
-            print("Better luck next time. Player busted")
+        cmd = ""                                    # this is where you play blackjack
+        while cmd != "Stay":
+            bust = 0
+            cmd = input("Hit or Stay?")
+            
+            if cmd == "Hit":
+                bust = self.player.hit()
+                self.player.show()
+            if bust == 1:
+                print("Better luck next time. Player busted")
             return 1
-        
-        while self.dealer.check_score() < 17:            # dealer hits until they get 17 or over
-            if self.dealer.hit() == 1:
+            
+            while self.dealer.check_score() < 17:            # dealer hits until they get 17 or over
+                if self.dealer.hit() == 1:
+                    self.dealer.show()
+                    print("Dealer busted. You win!")
+                    return 1
                 self.dealer.show()
-                print("Dealer busted. You win!")
-                return 1
-            self.dealer.show()
-            
-        if self.dealer.check_score() == self.player.check_score():
-            print("Tie - oh well")
-        elif self.dealer.check_score() > self.player.check_score():
-            print("Dealer's game.")
-        elif self.dealer.check_score() < self.player.check_score():
-            print("Player wins! Congrats!")
-            
+                
+            if self.dealer.check_score() == self.player.check_score():
+                print("Tie - oh well")
+            elif self.dealer.check_score() > self.player.check_score():
+                print("Dealer's game.")
+            elif self.dealer.check_score() < self.player.check_score():
+                print("Player wins! Congrats!")
+                
 b = Blackjack()
 b.play()
--- blackjack/deck.py
+++ blackjack/deck.py
@@ -6,13 +6,13 @@ class Deck:
         self.cards = []                         # initialize deck with empty list of cards
         
     def fiftytwo(self):                         # create 52 card deck
-        for crd1 in range(1,14):                # loop through 52 times
-            for crd2 in range(4):
-                self.cards.append(Card(crd1, crd2))
+        for i in range(1,14):                # loop through 52 times
+            for j in range(4):
+                self.cards.append(Card(i, j))
                 
     def draw(self, iteration):                  # randomly remove cards from deck
         cards = []                              # put cards in list so we can see them
-        for crd1 in range(iteration):
+        for i in range(iteration):
             card = random.choice(self.cards)
             self.cards.remove(card)
             cards.append(cards)
